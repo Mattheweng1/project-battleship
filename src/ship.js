c6c7ffb87bd570d71f049f45b372962c12bf2d81
Matthew Eng <129238647+Mattheweng1@users.noreply.github.com>
2024-02-06 18:09:40 -0600

    Refactor ship.js to include coordinates & name

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,39 @@
-function createShip(length) {
+function createShip(start, end, length, name) {
+  if (
+    (start[0] === end[0] && start[1] === end[1]) ||
+    (start[0] !== end[0] && start[1] !== end[1])
+  ) {
+    throw new Error("Coordinates must be vertically or horizontally in line");
+  } else if (
+    length !==
+    Math.abs(start[0] - end[0]) + Math.abs(start[1] - end[1]) + 1
+  ) {
+    throw new Error(`Ship length must be ${length}`);
+  }
+
+  const coords = [];
+
+  if (start[0] < end[0]) {
+    for (let i = start[0]; i <= end[0]; i++) {
+      coords.push([i, start[1]]);
+    }
+  } else if (end[0] < start[0]) {
+    for (let i = end[0]; i <= start[0]; i++) {
+      coords.push([i, end[1]]);
+    }
+  } else if (start[1] < end[1]) {
+    for (let i = start[1]; i <= end[1]; i++) {
+      coords.push([start[0], i]);
+    }
+  } else if (end[1] < start[1]) {
+    for (let i = end[1]; i <= start[1]; i++) {
+      coords.push([end[0], i]);
+    }
+  }
+
   return {
+    name,
+    coords,
     length,
     hitsTaken: 0,
     isSunk: function () {
--- src/ship.test.js
+++ src/ship.test.js
@@ -3,7 +3,7 @@ import { createShip } from "./ship";
 // Ship can take hits until it is sunk.
 
 test("hit() increments hitsTaken", () => {
-  const testShip = createShip(4);
+  const testShip = createShip([0, 0], [0, 4], 5, "test");
   testShip.hit();
   expect(testShip.hitsTaken).toBe(1);
   testShip.hit();
@@ -13,12 +13,14 @@ test("hit() increments hitsTaken", () => {
 });
 
 test("isSunk() returns false before taking any hits", () => {
-  const testShip = createShip(4);
+  const testShip = createShip([0, 0], [0, 4], 5, "test");
   expect(testShip.isSunk()).toBe(false);
 });
 
 test("isSunk() returns false until it's sunk", () => {
-  const testShip = createShip(4);
+  const testShip = createShip([0, 0], [0, 4], 5, "test");
+  testShip.hit();
+  expect(testShip.isSunk()).toBe(false);
   testShip.hit();
   expect(testShip.isSunk()).toBe(false);
   testShip.hit();
@@ -30,7 +32,7 @@ test("isSunk() returns false until it's sunk", () => {
 });
 
 test("hit() stops incrementing hitsTaken after isSunk() returns true", () => {
-  const testShip = createShip(4);
+  const testShip = createShip([0, 0], [0, 4], 5, "test");
   testShip.hit();
   expect(testShip.hitsTaken).toBe(1);
   testShip.hit();
@@ -40,8 +42,49 @@ test("hit() stops incrementing hitsTaken after isSunk() returns true", () => {
   expect(testShip.isSunk()).toBe(false);
   testShip.hit();
   expect(testShip.hitsTaken).toBe(4);
+  expect(testShip.isSunk()).toBe(false);
+  testShip.hit();
+  expect(testShip.hitsTaken).toBe(5);
   expect(testShip.isSunk()).toBe(true);
   testShip.hit();
-  expect(testShip.hitsTaken).toBe(4);
+  expect(testShip.hitsTaken).toBe(5);
   expect(testShip.isSunk()).toBe(true);
+});
+
+test("Ship has the name given to it", () => {
+  const testShip = createShip([0, 0], [0, 4], 5, "test");
+  expect(testShip.name).toBe("test");
+});
+
+test("Ship generates correct coords horizontally", () => {
+  const testShip = createShip([0, 0], [0, 4], 5, "test");
+  expect(testShip.coords).toEqual([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [0, 4],
+  ]);
+});
+
+test("Ship generates correct coords vertically", () => {
+  const testShip = createShip([3, 4], [0, 4], 4, "test");
+  expect(testShip.coords).toEqual([
+    [0, 4],
+    [1, 4],
+    [2, 4],
+    [3, 4],
+  ]);
+});
+
+test("Coordinates not in line throw an error", () => {
+  expect(() => createShip([1, 1], [4, 4], 4, "test")).toThrow(
+    "Coordinates must be vertically or horizontally in line"
+  );
+});
+
+test("Coordinates must create the correct length for the ship", () => {
+  expect(() => createShip([1, 1], [1, 4], 17, "test")).toThrow(
+    "Ship length must be 17"
+  );
 });
